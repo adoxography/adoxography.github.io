dae32db76699f11c4bdf0c298c5a6e631562cb90
adoxography <nevermore667@gmail.com>
2020-02-13 20:17:01 -0600

    Revamp site

--- _includes/tailwind.config.js
+++ _includes/tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   theme: {
-    extend: {}
+    fontFamily: {
+      display: ['Raleway', 'sans-serif'],
+      body: ['"Open Sans"', 'sans-serif']
+    },
+    extend: {
+      transitionProperty: {
+        spacing: 'margin, padding'
+      }
+    }
   },
-  variants: {},
+  variants: {
+    padding: ['responsive', 'hover'],
+    margin: ['responsive', 'hover']
+  },
   plugins: []
-}
+};
