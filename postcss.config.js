b6d6170cbf17994605323a18a080c4fb4b437b44
adoxography <nevermore667@gmail.com>
2020-04-17 10:55:18 -0500

    Shrink CSS output

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,21 @@
-const tailwindcss = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
+
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: [
+    './public/**/*.html',
+    './src/**/*.js'
+  ],
+
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+});
+
+const cssnano = require('cssnano')({ preset: 'default' });
 
 module.exports = {
   plugins: [
-    tailwindcss('./tailwind.js'),
-    require('autoprefixer')
+    require('tailwindcss'),
+    ...process.env.NODE_ENV === 'production'
+      ? [ autoprefixer, cssnano, purgecss]
+      : []
   ]
 };
